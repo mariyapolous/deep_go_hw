1c207b739d9bd07c3a795184e59434df4c98b82e
mariyapolous <mariyapolous@yandex.ru>
2025-09-01 01:48:58 +0500

    hw 6 deep go

--- homework/structs/homework_test.go
+++ homework/structs/homework_test.go
@@ -12,79 +12,124 @@ type Option func(*GamePerson)
 
 func WithName(name string) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		for i := range person.name {
+			person.name[i] = 0
+		}
+		copy(person.name[:], name)
 	}
 }
 
 func WithCoordinates(x, y, z int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.x, person.y, person.z = int32(x), int32(y), int32(z)
 	}
 }
 
 func WithGold(gold int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.gold = int32(gold)
 	}
 }
 
 func WithMana(mana int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		if mana < 0 {
+			mana = 0
+		}
+		if mana > 1000 {
+			mana = 1000
+		}
+		person.setBits(offMana, widthMana, uint64(mana))
 	}
 }
 
 func WithHealth(health int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		if health < 0 {
+			health = 0
+		}
+		if health > 1000 {
+			health = 1000
+		}
+		person.setBits(offHealth, widthHealth, uint64(health))
 	}
 }
 
 func WithRespect(respect int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		if respect < 0 {
+			respect = 0
+		}
+		if respect > 10 {
+			respect = 10
+		}
+		person.setBits(offRespect, widthSmall, uint64(respect))
 	}
 }
 
 func WithStrength(strength int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		if strength < 0 {
+			strength = 0
+		}
+		if strength > 10 {
+			strength = 10
+		}
+		person.setBits(offStrength, widthSmall, uint64(strength))
 	}
 }
 
 func WithExperience(experience int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		if experience < 0 {
+			experience = 0
+		}
+		if experience > 10 {
+			experience = 10
+		}
+		person.setBits(offExperience, widthSmall, uint64(experience))
 	}
 }
 
 func WithLevel(level int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		if level < 0 {
+			level = 0
+		}
+		if level > 10 {
+			level = 10
+		}
+		person.setBits(offLevel, widthSmall, uint64(level))
 	}
 }
 
 func WithHouse() func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.setBits(offHouse, 1, 1)
 	}
 }
 
 func WithGun() func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.setBits(offGun, 1, 1)
 	}
 }
 
 func WithFamily() func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		person.setBits(offFamily, 1, 1)
 	}
 }
 
 func WithType(personType int) func(*GamePerson) {
 	return func(person *GamePerson) {
-		// need to implement
+		if personType < 0 {
+			personType = 0
+		}
+		if personType > 3 {
+			personType = 3
+		}
+		person.setBits(offType, widthType, uint64(personType))
 	}
 }
 
@@ -92,92 +137,139 @@ const (
 	BuilderGamePersonType = iota
 	BlacksmithGamePersonType
 	WarriorGamePersonType
+	widthMana   = 10
+	widthHealth = 10
+	widthSmall  = 4
+	widthType   = 2
+
+	offMana       = 0
+	offHealth     = offMana + widthMana
+	offRespect    = offHealth + widthHealth
+	offStrength   = offRespect + widthSmall
+	offExperience = offStrength + widthSmall
+	offLevel      = offExperience + widthSmall
+	offHouse      = offLevel + widthSmall
+	offGun        = offHouse + 1
+	offFamily     = offGun + 1
+	offType       = offFamily + 1
 )
 
+func mask(width uint) uint64 {
+	return (uint64(1) << width) - 1
+}
+
+func (p *GamePerson) getPacked() uint64 {
+	return uint64(p.packed[0]) |
+		uint64(p.packed[1])<<8 |
+		uint64(p.packed[2])<<16 |
+		uint64(p.packed[3])<<24 |
+		uint64(p.packed[4])<<32 |
+		uint64(p.packed[5])<<40
+}
+
+func (p *GamePerson) setPacked(v uint64) {
+	p.packed[0] = byte(v)
+	p.packed[1] = byte(v >> 8)
+	p.packed[2] = byte(v >> 16)
+	p.packed[3] = byte(v >> 24)
+	p.packed[4] = byte(v >> 32)
+	p.packed[5] = byte(v >> 40)
+}
+
+func (p *GamePerson) setBits(off, width uint, val uint64) {
+	all := p.getPacked()
+	m := mask(width) << off
+	all = (all &^ m) | ((val & mask(width)) << off)
+	p.setPacked(all)
+}
+
+func (p *GamePerson) getBits(off, width uint) uint64 {
+	return (p.getPacked() >> off) & mask(width)
+}
+
 type GamePerson struct {
-	// need to implement
+	name   [42]byte
+	packed [6]byte
+
+	x    int32
+	y    int32
+	z    int32
+	gold int32
 }
 
 func NewGamePerson(options ...Option) GamePerson {
-	// need to implement
-	return GamePerson{}
+	person := GamePerson{}
+	for _, option := range options {
+		option(&person)
+	}
+	return person
 }
 
 func (p *GamePerson) Name() string {
-	// need to implement
-	return ""
+	i := 0
+	for ; i < len(p.name); i++ {
+		if p.name[i] == 0 {
+			break
+		}
+	}
+	return string(p.name[:i])
 }
 
 func (p *GamePerson) X() int {
-	// need to implement
-	return 0
+	return int(p.x)
 }
 
 func (p *GamePerson) Y() int {
-	// need to implement
-	return 0
+	return int(p.y)
 }
 
 func (p *GamePerson) Z() int {
-	// need to implement
-	return 0
+	return int(p.z)
 }
 
 func (p *GamePerson) Gold() int {
-	// need to implement
-	return 0
+	return int(p.gold)
 }
 
 func (p *GamePerson) Mana() int {
-	// need to implement
-	return 0
+	return int(p.getBits(offMana, widthMana))
 }
 
 func (p *GamePerson) Health() int {
-	// need to implement
-	return 0
+	return int(p.getBits(offHealth, widthHealth))
 }
 
 func (p *GamePerson) Respect() int {
-	// need to implement
-	return 0
+	return int(p.getBits(offRespect, widthSmall))
 }
 
 func (p *GamePerson) Strength() int {
-	// need to implement
-	return 0
+	return int(p.getBits(offStrength, widthSmall))
 }
 
 func (p *GamePerson) Experience() int {
-	// need to implement
-	return 0
+	return int(p.getBits(offExperience, widthSmall))
 }
 
 func (p *GamePerson) Level() int {
-	// need to implement
-	return 0
+	return int(p.getBits(offLevel, widthSmall))
 }
 
 func (p *GamePerson) HasHouse() bool {
-	// need to implement
-	return false
+	return p.getBits(offHouse, 1) != 0
 }
 
 func (p *GamePerson) HasGun() bool {
-	// need to implement
-	return false
+	return p.getBits(offGun, 1) != 0
 }
 
 func (p *GamePerson) HasFamilty() bool {
-	// need to implement
-	return false
+	return p.getBits(offFamily, 1) != 0
 }
 
 func (p *GamePerson) Type() int {
-	// need to implement
-	return 0
+	return int(p.getBits(offType, widthType))
 }
-
 func TestGamePerson(t *testing.T) {
 	assert.LessOrEqual(t, unsafe.Sizeof(GamePerson{}), uintptr(64))
 
