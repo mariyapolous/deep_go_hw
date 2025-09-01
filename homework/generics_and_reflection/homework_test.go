e1f71f8f92cb72a77d433f2cb546b38b4634d600
mariyapolous <mariyapolous@yandex.ru>
2025-09-01 22:21:07 +0500

    hw 9 deep go

--- homework/generics_and_reflection/homework_test.go
+++ homework/generics_and_reflection/homework_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"reflect"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -16,8 +19,61 @@ type Person struct {
 }
 
 func Serialize(person Person) string {
-	// need to implement
-	return ""
+	v := reflect.ValueOf(person)
+	t := reflect.TypeOf(person)
+
+	var b strings.Builder
+
+	for i := 0; i < v.NumField(); i++ {
+		field := t.Field(i)
+		value := v.Field(i)
+
+		tag := field.Tag.Get("properties")
+		if tag == "" {
+			continue
+		}
+
+		parts := strings.Split(tag, ",")
+		key := parts[0]
+		omitEmpty := len(parts) > 1 && parts[1] == "omitempty"
+
+		if omitEmpty && isEmptyValue(value) {
+			continue
+		}
+
+		var strVal string
+		switch value.Kind() {
+		case reflect.String:
+			strVal = value.String()
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			strVal = strconv.FormatInt(value.Int(), 10)
+		case reflect.Bool:
+			strVal = strconv.FormatBool(value.Bool())
+		default:
+			strVal = ""
+		}
+
+		if b.Len() > 0 {
+			b.WriteRune('\n')
+		}
+		b.WriteString(key)
+		b.WriteRune('=')
+		b.WriteString(strVal)
+	}
+
+	return b.String()
+}
+
+func isEmptyValue(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.String, reflect.Array, reflect.Slice, reflect.Map:
+		return v.Len() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	}
+	return false
 }
 
 func TestSerialization(t *testing.T) {
