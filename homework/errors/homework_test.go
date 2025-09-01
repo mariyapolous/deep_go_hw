43dc249ad67328a31918ed156c8803d6378dbb40
mariyapolous <mariyapolous@yandex.ru>
2025-09-01 22:15:14 +0500

    hw 8 deep go

--- homework/errors/homework_test.go
+++ homework/errors/homework_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,19 +12,50 @@ import (
 // go test -v homework_test.go
 
 type MultiError struct {
-	// need to implement
+	Errors []error
 }
 
 func (e *MultiError) Error() string {
-	// need to implement
-	return ""
+	if e == nil || len(e.Errors) == 0 {
+		return ""
+	}
+
+	var b strings.Builder
+	b.WriteString(fmt.Sprintf("%d errors occured:\n", len(e.Errors)))
+
+	for _, err := range e.Errors {
+		b.WriteString("\t* ")
+		b.WriteString(err.Error())
+	}
+	b.WriteString("\n")
+
+	return b.String()
 }
 
 func Append(err error, errs ...error) *MultiError {
-	// need to implement
-	return nil
-}
+	var me *MultiError
 
+	if err == nil {
+		me = &MultiError{}
+	} else {
+		if existing, ok := err.(*MultiError); ok {
+			me = existing
+		} else {
+			me = &MultiError{Errors: []error{err}}
+		}
+	}
+
+	for _, e := range errs {
+		if e != nil {
+			me.Errors = append(me.Errors, e)
+		}
+	}
+
+	if len(me.Errors) == 0 {
+		return nil
+	}
+	return me
+}
 func TestMultiError(t *testing.T) {
 	var err error
 	err = Append(err, errors.New("error 1"))
